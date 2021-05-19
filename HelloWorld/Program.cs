19303836536a0563f773cabe20a41fe5fe7e0ce9
Alex Rauenzahn <xandermv@yahoo.com>
2021-05-19 14:08:56 -0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Alex Rauenzahn
+ * CIS-1280
+ * May 19th, 2021
+ */
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
